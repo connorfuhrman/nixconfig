17be7705f6f8eff7273b7fb85cd75a90bdb25415
Connor Fuhrman <connormfuhrman@gmail.com>
2025-01-17 22:04:52 -0800

    Brought both configs for Air and Mini into same function and added attribute set

--- flake.nix
+++ flake.nix
@@ -4,8 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
 
-    darwin.url = "github:lnl7/nix-darwin/nix-darwin-24.11";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin = {
+      url = "github:lnl7/nix-darwin/nix-darwin-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
@@ -13,19 +15,26 @@
     };
   };
 
-  outputs = { self, darwin, nixpkgs, home-manager }: {
-    darwinConfigurations.Connors-MacBook-Air = darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules = [
-        ./configuration-darwin.nix
+  outputs = { self, darwin, nixpkgs, home-manager }:
+    let
+      mkBasicConfig = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          ./configuration-darwin.nix
         
-        home-manager.darwinModules.home-manager
-        {
-         home-manager.useGlobalPkgs = true;
-         home-manager.useUserPackages = true;
-         home-manager.users.connorfuhrman = import ./home.nix;
-        }
-      ];
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.connorfuhrman = import ./home.nix;
+          }
+        ];
+      };
+    in
+    {
+        darwinConfigurations = {
+          Connors-MacBook-Air = mkBasicConfig;
+          Connors-Mac-mini = mkBasicConfig;
+        };
     };
-  };
 }
